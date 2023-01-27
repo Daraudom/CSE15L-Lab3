5576a675dece717f01eb869af8d73e40b277cf5d
Daraudom <dnhem@ucsd.edu>
2023-01-26 21:58:01 -0800

    commit

--- ArrayExamples.java
+++ ArrayExamples.java
@@ -1,11 +1,20 @@
 
 
 public class ArrayExamples {
+  public static void main(String[] args) {
+    int[] intArray1 = {1, 2, 3, 4, 5, 6, 7};
+    ArrayExamples.reverseInPlace(intArray1);
+    for (int i: intArray1) { System.out.println(i);}
+  }
 
   // Changes the input array to be in reversed order
   static void reverseInPlace(int[] arr) {
+    int[] correctResult = new int[arr.length];
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = arr[arr.length - i - 1];
+      correctResult[i] = arr[arr.length - i - 1];
+     }
+    for (int i = 0; i < arr.length; i++){
+      arr[i] = correctResult[i];
     }
   }
 
@@ -14,15 +23,16 @@ public class ArrayExamples {
   static int[] reversed(int[] arr) {
     int[] newArray = new int[arr.length];
     for(int i = 0; i < arr.length; i += 1) {
-      arr[i] = newArray[arr.length - i - 1];
+      newArray[i] = arr[arr.length - i - 1];
     }
-    return arr;
+    return newArray;
   }
 
   // Averages the numbers in the array (takes the mean), but leaves out the
   // lowest number when calculating. Returns 0 if there are no elements or just
   // 1 element in the array
   static double averageWithoutLowest(double[] arr) {
+    int lowestCount = 0;
     if(arr.length < 2) { return 0.0; }
     double lowest = arr[0];
     for(double num: arr) {
@@ -30,9 +40,14 @@ public class ArrayExamples {
     }
     double sum = 0;
     for(double num: arr) {
-      if(num != lowest) { sum += num; }
+      if(num != lowest) { 
+        sum += num; 
+      }
+      if (num == lowest) {
+        lowestCount++;
+      }
     }
-    return sum / (arr.length - 1);
+    return sum / (arr.length - lowestCount);
   }
 
 
---
+++ ListTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.*;
+
+public class ListTest {
+    @Test
+    public void testMerge () {
+        String[] string1 = {"a", "c", "d"};
+        Arraylist<String> input1 = new Arrays.asList(string1);
+
+        };
+    }
+
